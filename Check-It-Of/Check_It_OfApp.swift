f4c3e3c3c76b920559529e443b31bf3ed95cacca
Alexandre Samson <alexander_samson@icloud.com>
2024-02-07 09:25:32 +0100

    Initial Commit

---
+++ Check-It-Of/Check_It_OfApp.swift
@@ -0,0 +1,17 @@
+//
+//  Check_It_OfApp.swift
+//  Check-It-Of
+//
+//  Created by Alexandre Samson on 07.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Check_It_OfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Check-It-Of/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Check-It-Of
+//
+//  Created by Alexandre Samson on 07.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
